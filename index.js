4218443c7b32cf49321de0840fcf036a14104998
sony-nurdianto <sonynurdianto445@gmail.com>
2020-04-07 16:03:27 +0700

    ok aku pull

--- index.js
+++ index.js
@@ -10,11 +10,17 @@ fetch (`${link}`)
 })
 .then (data => {
     console.log(data)
-    console.log(`data longtitude ${data.coord.lon} data latitude${data.coord.lat}`)
-    console.log(`data weather = id : ${data.weather[0].id} main:${data.weather[0].main} descriptions:${data.weather[0].description} icons:${data.weather[0].icon}`)
-    console.log(`data base = ${data.base}`)
-    console.log(`data main = temperature :${data.main.temp} feels_like:${data.main.feels_like} temp_min:${data.main.temp_min} temp_max:${data.main.temp_max} pressure:${data.main.preesure} humidity:${ data.main.humidity}`)
-    console.log(`data country${data.sys.country}`)
-    console.log(`data ${data.name}`)
+    
+
+
+    document.getElementById('weather').innerHTML
+
 })
 
+// console.log(data)
+//     console.log(`data longtitude ${data.coord.lon} data latitude${data.coord.lat}`)
+//     console.log(`data weather = id : ${data.weather[0].id} main:${data.weather[0].main} descriptions:${data.weather[0].description} icons:${data.weather[0].icon}`)
+//     console.log(`data base = ${data.base}`)
+//     console.log(`data main = temperature :${data.main.temp} feels_like:${data.main.feels_like} temp_min:${data.main.temp_min} temp_max:${data.main.temp_max} pressure:${data.main.preesure} humidity:${ data.main.humidity}`)
+//     console.log(`data country${data.sys.country}`)
+//     console.log(`data ${data.name}`)
