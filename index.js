d8ad95792b8eff53a6d590acdac7b9887a505272
sony-nurdianto <sonynurdianto445@gmail.com>
2020-04-07 15:15:27 +0700

    ok data udah aku translate

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d'
 
 
+
+
 fetch (`${URL}`)
 .then (response =>{
     return response.json()
@@ -11,11 +13,9 @@ fetch (`${URL}`)
     console.log(`data longtitude ${data.coord.lon} data latitude${data.coord.lat}`)
     console.log(`data weather = id : ${data.weather[0].id} main:${data.weather[0].main} descriptions:${data.weather[0].description} icons:${data.weather[0].icon}`)
     console.log(`data base = ${data.base}`)
-console.log(`data main = temperature :${data.main.temp} feels_like:${data.main.feels_like} temp_min:${data.main.temp_min} temp_max:${data.main.temp_max} pressure:${data.main.preesure} humidity:${ data.main.humidity}`)
+    console.log(`data main = temperature :${data.main.temp} feels_like:${data.main.feels_like} temp_min:${data.main.temp_min} temp_max:${data.main.temp_max} pressure:${data.main.preesure} humidity:${ data.main.humidity}`)
+    console.log(`data country = ${data.sys.country}`)
+    console.log(`data kota ${data.name}`)
 })
-// temp: 304.43
-// feels_like: 306.87
-// temp_min: 302.04
-// temp_max: 306.15
-// pressure: 1008
-// humidity: 62
+
+// type: 1, id: 9383, country: "ID", sunrise: 1586213649, sunset: 1586256891}
