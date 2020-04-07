33bd12150d4b53131ab499c2dc831505d6746077
sony-nurdianto <sonynurdianto445@gmail.com>
2020-04-07 14:18:05 +0700

    fetching data

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d'
+
+
+fetch (`${URL}`)
+.then (response =>{
+    return response.json();
+    
+})
+.then (data => {
+    console.log(data)
+})
